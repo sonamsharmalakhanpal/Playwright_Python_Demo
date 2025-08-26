1bfffb4dd5f909eef4da26268f98f8e5c3fd0dac
Sonam Lakhanpal <sonamlakhanpal@gmail.com>
2025-08-26 13:27:01 +0530

    new changes on 26-08-2025

--- tests/UI/test_google.py
+++ tests/UI/test_google.py
@@ -1,10 +1,12 @@
 from playwright.sync_api import sync_playwright
 
-with sync_playwright() as p:
-    browser = p.chromium.launch(headless=False)
-    page = browser.new_page()
-    page.goto("https://google.com")
-    page.screenshot(path="example.png")
-    browser.close()
+def test_api_response():
+    with sync_playwright() as p:
+        browser = p.chromium.launch(headless=False)
+        page = browser.new_page()
+        page.goto("https://google.com")
+        page.screenshot(path="googletest.png")
+        browser.close()
 
-    # pytest ./tests/UI/test_google.py --browser=chromium --headed
+    # venv/Scripts/activate
+    # pytest ./tests/UI/test_google.py --browser=chromium --headed --video=on
